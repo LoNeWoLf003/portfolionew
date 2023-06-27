83015e950cb07d6d178c99f88a591dc75ff6093a
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-06-27 19:08:55 +0530

    skills

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/header/header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 const App = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const App = () => {
       <main className="main">
         <Home />
         <About />
+        <Skills />
       </main>
     </>
   );
