eab0b36d5e940b880a8591223228b1eae806f1a6
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-06-28 20:03:45 +0530

    testimony

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,30 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+  {
+    id: 1,
+    img: Image1,
+    title: "John Doe1",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  },
+  {
+    id: 2,
+    img: Image2,
+    title: "John Doe2",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  },
+  {
+    id: 3,
+    img: Image3,
+    title: "John Doe3",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  },
+  {
+    id: 2,
+    img: Image2,
+    title: "John Doe4",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  },
+];
