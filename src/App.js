7bed32933c8d527a96e022771fd6c3980a1e2df4
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-06-28 12:11:37 +0530

    qualification

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
+import Qualification from "./components/qualification/Qualification";
 const App = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const App = () => {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </>
   );
