05a3b6cd7be85500e4f062a6d9a57e539e98f101
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-06-29 22:29:31 +0530

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Sayak SenGupta</h1>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About Me
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+        <div className="footer__social">
+          <a
+            href="www.instagram.com"
+            className="footer__social-icon"
+            target="_blank"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+          <a
+            href="www.linkedin.com"
+            className="footer__social-icon"
+            target="_blank"
+          >
+            <i class="bx bxl-linkedin"></i>
+          </a>
+          <a
+            href="www.cloud.com"
+            className="footer__social-icon"
+            target="_blank"
+          >
+            <i class="bx bx-cloud"></i>
+          </a>
+        </div>
+        <span className="footer__copy">&#169; LoNeWoLf003. All Rights Reserved.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
