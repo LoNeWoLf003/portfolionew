eadcf5589d89dd8466500dd62396b53cd1c79fbe
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-07-02 12:24:22 +0530

    almost done

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,4 +1,4 @@
-import Image1 from "../../assets/testimonial1.png";
+import Image1 from "../../assets/arghyada.jpg";
 import Image2 from "../../assets/testimonial2.png";
 import Image3 from "../../assets/testimonial3.png";
 
