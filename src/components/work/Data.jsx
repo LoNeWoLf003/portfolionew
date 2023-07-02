d132e0d9c411a4e02514c4a8a88fa59ec8c53225
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-07-02 13:19:31 +0530

    oi kichhu ekta

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -8,31 +8,36 @@ export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: "Project 1",
+    title: "Samarth TMSL",
+    desc: "Website for the official educational society of Techno Main Salt Lake",
     category: "web",
   },
   {
     id: 2,
     image: Work2,
-    title: "Project 2",
+    title: "Grofy",
+    desc: "Android Grocery Delivery Application, available on Play Store.",
     category: "app",
   },
   {
     id: 3,
     image: Work3,
     title: "Project 3",
+    desc: "Project 1",
     category: "cloud",
   },
   {
     id: 4,
     image: Work4,
     title: "Project 4",
+    desc: "Project 1",
     category: "app",
   },
   {
     id: 5,
     image: Work5,
     title: "Project 5",
+    desc: "Project 1",
     category: "web",
   },
 ];
