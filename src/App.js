12972e5dc26f15f239397e97f1add2653ae0b8f7
Sayak SenGupta <91112848+LoNeWoLf003@users.noreply.github.com>
2023-07-02 10:04:09 +0530

    scroll

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Qualification from "./components/qualification/Qualification";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 const App = () => {
   return (
     <>
@@ -21,6 +22,7 @@ const App = () => {
         <Testimonials />
         <Contact />
         <Footer />
+        <ScrollUp />
       </main>
     </>
   );
