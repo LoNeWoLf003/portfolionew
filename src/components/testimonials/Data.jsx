9be947daff44f0a7a623727e455c4316eea1ad18
LoNeWoLf003 <sayaksengupta2110@gmail.com>
2024-03-21 20:09:43 +0530

    testi

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,5 +1,4 @@
 import Image1 from "../../assets/arghyada.jpg";
-import Image2 from "../../assets/testimonial2.png";
 import Image3 from "../../assets/testimonial3.png";
 
 export const Data = [
@@ -10,25 +9,33 @@ export const Data = [
     post: "SDE, ThoughtWorks",
     desc: "I highly recommend Sayak for any professional opportunity. I had the pleasure of working closely with him during his time as a Class Representative, and his leadership skills truly impressed me. Sayak efficiently managed not only his own responsibilities but also those of the entire class, demonstrating his exceptional ability to lead and co-ordinate a team.",
   },
+  // {
+  //   id: 2,
+  //   img: Image2,
+  //   title: "John Doe2",
+  //   post: "CEO",
+  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+  // },
   {
     id: 2,
-    img: Image2,
-    title: "John Doe2",
-    post: "CEO",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    img: Image3,
+    title: "Coming Soon!",
+    post: " ",
+    desc: "   ",
   },
   {
     id: 3,
     img: Image3,
-    title: "John Doe3",
-    post: "CEO",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    title: "Coming Soon!",
+    post: " ",
+    desc: "   ",
   },
   {
-    id: 2,
-    img: Image2,
-    title: "John Doe4",
-    post: "CEO",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    id: 4,
+    img: Image3,
+    title: "Coming Soon!",
+    post: " ",
+    desc: "   ",
   },
+
 ];
