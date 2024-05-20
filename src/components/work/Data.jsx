53097406b2edc38b6a2449859a852705a20feb52
LoNeWoLf003 <sayaksengupta2110@gmail.com>
2024-05-20 21:45:39 +0530

    update

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,7 +1,7 @@
 import Work1 from "../../assets/sam.png";
-import Work2 from "../../assets/grofy.jpg";
 import Work3 from "../../assets/cloud.png";
 import Work4 from "../../assets/rcs.jpg";
+import Work5 from "../../assets/stock.avif";
 export const projectsData = [
   {
     id: 1,
@@ -9,25 +9,15 @@ export const projectsData = [
     title: "Samarth TMSL",
     desc: "Website for the official educational society of Techno Main Salt Lake",
     category: "web",
-    link: "https://www.samarthtmsl.live/"
+    link: "https://www.samarthtmsl.live/",
   },
-  // {
-  //   id: 2,
-  //   image: Work2,
-  //   title: "Grofy",
-  //   desc: "Android Grocery Delivery Application, available on Play Store.",
-  //   category: "app",
-  //   link: "https://play.google.com/store/apps/details?id=com.grofy.grofy&pli=1"
-
-  // },
   {
     id: 2,
     image: Work3,
     title: "Google Cloud Arcade Facilitator Program",
     desc: "Co-Facilitator for Cohort-2 of the Google Cloud Arcade Facilitator Program",
     category: "cloud",
-    link: "https://www.cloudskillsboost.google/public_profiles/dd03d123-b7ad-4c8d-bccb-7a652dd5ce4e/badges/4244511"
-
+    link: "https://www.cloudskillsboost.google/public_profiles/dd03d123-b7ad-4c8d-bccb-7a652dd5ce4e/badges/4244511",
   },
   {
     id: 3,
@@ -35,22 +25,28 @@ export const projectsData = [
     title: "Right Click Systems",
     desc: "Website Building for Right Click Systems, Kolkata",
     category: "web",
-    link: "https://rightclicksystems.vercel.app/"
-
+    link: "https://rightclicksystems.vercel.app/",
   },
-
+  {
+    id: 4,
+    image: Work5,
+    title: "Stock Prediction",
+    desc: "Website to predict future stock prices using ML (LSTM, Tensorflow)",
+    category: "web",
+    link: "https://trialstock.streamlit.app/",
+  },
 ];
 export const projectsNav = [
-    {
-        name:'all',
-    },
-    {
-        name:'web',
-    },
-    // {
-    //     name:'app',
-    // },
-    {
-        name:'cloud',
-    },
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  // {
+  //     name:'app',
+  // },
+  {
+    name: "cloud",
+  },
 ];
